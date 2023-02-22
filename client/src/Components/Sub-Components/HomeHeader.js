c764c355a783f85fd5bc37f041223c80c3a81564
Carlos Macario <cmacariovx@gmail.com>
2023-02-21 20:59:36 -0500

    home header rough complete

--- client/src/Components/Sub-Components/HomeHeader.js
+++ client/src/Components/Sub-Components/HomeHeader.js
@@ -3,6 +3,7 @@ import React from "react";
 import './HomeHeader.css'
 
 import airbnbLogo from '../../Images/airbnbLogo.png'
+import personalPic from '../../Images/personalPic.jpg'
 
 function HomeHeader() {
     return (
@@ -28,10 +29,20 @@ function HomeHeader() {
                     </div>
                 </div>
                 <div className="homeHeader1RightContainer">
-
+                    <div className="homeHeader1AddHomeContainer">
+                        <p className="homeHeader1AddHomeText">Airbnb your home</p>
+                    </div>
+                    <div className="homeHeader1UserContainer">
+                        <i className="fa-solid fa-bars homeHeader1UserMenuBars"></i>
+                        <img src={personalPic} className="homeHeader1UserPicture"></img>
+                    </div>
                 </div>
             </div>
             <div className="homeHeader2Container">
+
+            </div>
+            {/* if logged in, display this div with the real price switch */}
+            <div className="homeHeader3Container">
 
             </div>
         </div>
