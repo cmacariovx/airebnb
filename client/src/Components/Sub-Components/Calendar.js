9dbf5bfc8ae23893fabdf04ee62bf89512ba4d5d
Carlos Macario <cmacariovx@gmail.com>
2023-03-01 17:30:53 -0500

    small changes

--- client/src/Components/Sub-Components/Calendar.js
+++ client/src/Components/Sub-Components/Calendar.js
@@ -7,15 +7,19 @@ function Calendar() {
         <div className="calendarContainer">
             <div className="calendarMonthContainer">
                 <div className="calendarLeftMonthContainer">
+                    <p className="calendarLeftMonthText">March 2023</p>
                     <div className="calendarLeftArrowContainer">
-                        <i className="fa-solid fa-chevron-left calendarLeftArrow"></i>
+                        <div className="calendarLeftArrowCircleContainer">
+                            <i className="fa-solid fa-chevron-left calendarLeftArrow"></i>
+                        </div>
                     </div>
-                    <p className="calendarMonthText">March 2023</p>
                 </div>
                 <div className="calendarRightMonthContainer">
-                    <p className="calendarMonthText">April 2023</p>
+                    <p className="calendarRightMonthText">April 2023</p>
                     <div className="calendarRightArrowContainer">
-                        <i className="fa-solid fa-chevron-right calendarRightArrow"></i>
+                        <div className="calendarRightArrowCircleContainer">
+                            <i className="fa-solid fa-chevron-right calendarRightArrow"></i>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -80,7 +84,60 @@ function Calendar() {
                 </div>
                 <div className="calendarRightBodyContainer">
                     <table className="calendarRight">
-
+                        <thead>
+                            <tr>
+                                <th>Su</th>
+                                <th>Mo</th>
+                                <th>Tu</th>
+                                <th>We</th>
+                                <th>Th</th>
+                                <th>Fr</th>
+                                <th>Sa</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                                <td>7</td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>9</td>
+                                <td>10</td>
+                                <td>11</td>
+                                <td>12</td>
+                                <td>13</td>
+                                <td>14</td>
+                            </tr>
+                            <tr>
+                                <td>15</td>
+                                <td>16</td>
+                                <td>17</td>
+                                <td>18</td>
+                                <td>19</td>
+                                <td>20</td>
+                                <td>21</td>
+                            </tr>
+                            <tr>
+                                <td>22</td>
+                                <td>23</td>
+                                <td>24</td>
+                                <td>25</td>
+                                <td>26</td>
+                                <td>27</td>
+                                <td>28</td>
+                            </tr>
+                            <tr>
+                                <td>29</td>
+                                <td>30</td>
+                                <td>31</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
