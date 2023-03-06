bbdf2e75655c722d64605501f81a42948dcb76c7
Carlos Macario <cmacariovx@gmail.com>
2023-03-06 17:37:58 -0500

    profile rough complete

--- client/src/Components/Pages/Profile.js
+++ client/src/Components/Pages/Profile.js
@@ -4,12 +4,14 @@ import './Profile.css'
 
 import HomeHeader from "../Sub-Components/HomeHeader";
 import ProfileBody from "../Sub-Components/ProfileBody";
+import HomeFooter from "../Sub-Components/HomeFooter";
 
 function Profile() {
     return (
         <div className="profileContainer">
             <HomeHeader homePage={false} profilePage={true}/>
             <ProfileBody/>
+            <HomeFooter homePage={false}/>
         </div>
     )
 }
